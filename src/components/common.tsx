fb24c1f14a69330dd3f8b6279e525140624feedc
GJ <lgj9172@gmail.com>
2021-01-13 15:00:57 +0900

    update layout css

--- src/components/common.tsx
+++ src/components/common.tsx
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    box-sizing: border-box;
     max-width: 600px;
-    height: 100vh;
+    min-height: 100vh;
     margin: 0px auto;
     padding: 10px;
-    /* border: solid 1px black; */
-    /* background: #ebebeb; */
-`;
+`
