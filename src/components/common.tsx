0a8bf98133cab49fa59d81d7ec59b4c2b75294db
GJ <lgj9172@gmail.com>
2021-01-14 15:34:26 +0900

    add tag component

--- src/components/common.tsx
+++ src/components/common.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@material-ui/core";
 import styled from "styled-components";
 
 export const Container = styled.div`
@@ -6,4 +7,11 @@ export const Container = styled.div`
     min-height: 100vh;
     margin: 0px auto;
     padding: 10px;
-`
+`;
+
+export const Tag = styled(Box)`
+    display: inline-flex;
+    border-radius: 5px;
+    padding: 2px 7px;
+    color: #FFFFFF;
+`;
